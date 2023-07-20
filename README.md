bb66e625af8af50145f4a98f9ebae323308b2596
rikear <135054825+rikear@users.noreply.github.com>
2023-07-20 01:04:25 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# nextjs-blog
-Practice only guys
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/new.css';
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
